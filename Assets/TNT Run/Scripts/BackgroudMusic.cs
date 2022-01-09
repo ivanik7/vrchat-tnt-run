84bbc12256f83b8c5a4ee7b129432997f41ade23
ivanik <ivan170102@gmail.com>
2022-01-09 21:32:57 +0500

    Add music

---
+++ Assets/TNT Run/Scripts/BackgroudMusic.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class BackgroudMusic : UdonSharpBehaviour
+{
+    public AudioClip[] audioClips;
+    AudioSource audioSource;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate() {
+        if (!audioSource.isPlaying) {
+            int track = Random.Range(0, audioClips.Length);
+            audioSource.clip = audioClips[track];
+            audioSource.Play();
+        }
+    }
+}
